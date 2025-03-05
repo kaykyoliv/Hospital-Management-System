90c4f9f05419de506f3c6214c319348aba2013d0
Kayky Oliveira <kaykydeeoliveira@gmail.com>
2025-03-05 10:38:57 -0300

    Model User and Employee

---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Employee extends User{
+
+    private double salary;
+
+    public Employee(){
+        super();
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
